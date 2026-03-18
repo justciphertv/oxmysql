febb203673e59d37bee421b5fffc6419798a9e8c
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2026-03-18 18:45:18 +0100

    Remove unused files

--- src/utils/validateResultSet.ts
+++
@@ -1,9 +0,0 @@
-const oversizedResultSet = GetConvarInt('mysql_resultset_warning', 1000);
-
-export default function (invokingResource: string, query: string, rows: any) {
-  const length = Array.isArray(rows) ? rows.length : 0;
-
-  if (length < oversizedResultSet) return;
-
-  console.warn(`${invokingResource} executed a query with an oversized result set (${length} results)!\n${query}`);
-}
